ed4c4d3cf8e1e258108049118bbbfe58d91a1dcf
udahjay <judahjaimeslatter@gmail.com>
2021-02-01 19:55:59 +1100

    Added index scripts and styles

---
+++ script.js
@@ -0,0 +1,119 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  $("#lengthModal").modal("show");
+  alertMessage.style.display = "none";
+
+  //   var password = generatePassword();
+  //   var passwordText = document.querySelector("#password");
+  //   passwordText.value = password;
+}
+//
+
+var lengthNextButton = document.querySelector("#lengthNextButton");
+
+function handleClick() {
+  var minInput = document.querySelector("#minInput").value;
+  var maxInput = document.querySelector("#maxInput").value;
+  var numericInput = document.querySelector("#numericCheckbox").checked;
+  var upperInput = document.querySelector("#upperCheckbox").checked;
+  var lowerInput = document.querySelector("#lowerCheckbox").checked;
+  var specialInput = document.querySelector("#specialCheckbox").checked;
+  var alertMessage = document.querySelector("#alertMessage");
+
+  if (!numericInput && !upperInput && !lowerInput && !specialInput) {
+    alertMessage.style.display = "block";
+
+    return;
+  }
+
+  //returns valid numbers from string
+  var minNumber = parseInt(minInput, 10);
+
+  var maxNumber = parseInt(maxInput, 10);
+
+  if (!minNumber || !maxNumber) {
+    alertMessage.style.display = "block";
+
+    return;
+  }
+  if (minNumber > maxNumber) {
+    alertMessage.style.display = "block";
+    return;
+  }
+
+  if (minNumber < 8 || maxNumber > 128) {
+    alertMessage.style.display = "block";
+    return;
+  }
+
+  // function to
+  var securePassword = generatePassword(
+    minNumber,
+    maxNumber,
+    specialInput,
+    numericInput,
+    upperInput,
+    lowerInput
+  );
+
+  var passwordText = document.querySelector("#password");
+  passwordText.value = securePassword;
+
+  $("#lengthModal").modal("hide");
+  console.log(securePassword);
+}
+// add event listener to
+lengthNextButton.addEventListener("click", handleClick);
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// copied from mdn docs to create a number randomizer https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+// creats string values of characters
+let lower = "abcdefghijklmnopqrstuvwxyz";
+let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let special = "!@#$%^&*()_+|~`;'][";
+let numeric = "1234567890";
+
+// make a function that generates a random password
+function generatePassword(
+  min,
+  max,
+  allowLower,
+  allowUpper,
+  allowSpecial,
+  allowNumeric
+) {
+  let allowedChars = "";
+
+  if (allowLower) {
+    allowedChars += lower;
+  }
+  if (allowUpper) {
+    allowedChars += upper;
+  }
+  if (allowNumeric) {
+    allowedChars += numeric;
+  }
+  if (allowSpecial) {
+    allowedChars += special;
+  }
+
+  //password length
+  let passwordLength = getRandomInt(max - min) + min;
+  let result = "";
+  for (let i = 0; i < passwordLength; i++) {
+    let randomNumber = getRandomInt(allowedChars.length);
+    let randomChar = allowedChars.charAt(randomNumber);
+
+    result += randomChar;
+  }
+  console.log(passwordLength);
+  return result;
+}
